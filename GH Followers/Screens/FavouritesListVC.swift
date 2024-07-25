6370e2dd7c2f9fd06859597c500f180e20481ad2
swaruppanda11 <swaruppanda11@gmail.com>
2024-07-25 07:33:06 +0530

    added delete favorite functionality

--- GH Followers/Screens/FavouritesListVC.swift
+++ GH Followers/Screens/FavouritesListVC.swift
@@ -84,4 +84,24 @@ extension FavouritesListVC: UITableViewDataSource, UITableViewDelegate {
         destVC.title = favorite.login
         navigationController?.pushViewController(destVC, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {
+            return
+        }
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistanceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else {
+                return
+            }
+            
+            guard let error = error else {
+                return
+            }
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "OK")
+        }
+    }
 }
