cba52b7368760c600695fc460460d89c15eb1df0
swaruppanda11 <swaruppanda11@gmail.com>
2024-08-14 11:03:06 +0530

    added a minor change

--- GH Followers/Screens/UserInfoVC.swift
+++ GH Followers/Screens/UserInfoVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol UserInfoVCDelegate: class {
+protocol UserInfoVCDelegate: Any {
     func didTapGithubProfile(for user: User)
     func didTapGetFollowers(for user: User)
 }
